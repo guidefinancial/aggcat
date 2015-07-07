0d6b06daf9e1d5fc38f2a08552628ee1c7167b77
scott@guidefinancial.com <scott@guidefinancial.com>
2015-07-06 19:34:03 -0700

    Fix typo on certificate_value key

--- lib/aggcat/configurable.rb
+++ lib/aggcat/configurable.rb
@@ -1,7 +1,7 @@
 module Aggcat
   module Configurable
 
-    attr_writer :issuer_id, :consumer_key, :consumer_secret, :certificate_path, :certificat_value, :customer_id, :open_timeout, :read_timeout, :verbose, :private_key_string
+    attr_writer :issuer_id, :consumer_key, :consumer_secret, :certificate_path, :certificate_value, :customer_id, :open_timeout, :read_timeout, :verbose, :private_key_string
 
     KEYS = [:issuer_id, :consumer_key, :consumer_secret, :certificate_path, :certificate_value, :customer_id, :open_timeout, :read_timeout, :verbose, :private_key_string]
 
